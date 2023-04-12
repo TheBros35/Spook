357b24fb528a32dc5cd6ae92a933f39bee9875ce
Brandon Ambrose <bpambrose97@gmail.com>
2023-04-12 19:22:43 -0400

    created computer model and added one test computer

--- base/models.py
+++ base/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Computer(models.Model):
+    mac_address = models.CharField(max_length=17)
+    name = models.CharField(max_length=100)
