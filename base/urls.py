cf5bbaa7428e6aee861005a891eecd2f61e76646
Brandon Ambrose <bpambrose97@gmail.com>
2023-04-12 19:49:04 -0400

    added reqs, and now magic packets send!

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns =[
     path('', views.index),
+    path('send', views.send),
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from wakeonlan import send_magic_packet
+
 from .models import Computer
 
 
@@ -8,3 +10,9 @@ def index(request):
         'computers': computers,
     }
     return render(request=request, template_name='index.html', context=context)
+
+
+def send(request):
+    mac_address = request.GET['mac_address']
+    send_magic_packet(mac_address)
+    return redirect('/')
