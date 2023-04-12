e650e4ac64c7f1fcf815d404d4423b8385cc4f16
Brandon Ambrose <bpambrose97@gmail.com>
2023-04-12 19:42:15 -0400

    modified function to output all computer objects, as well as rendering them in index

--- base/views.py
+++ base/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Computer
 
-
-# Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    computers = Computer.objects.all()
+    context = {
+        'computers': computers,
+    }
+    return render(request=request, template_name='index.html', context=context)
