813fea5934c024a46ee3bed77b8b0f4781b9b821
Brandon Ambrose <bpambrose97@gmail.com>
2023-04-12 20:08:11 -0400

    corrected the add function

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns =[
     path('', views.index),
     path('send', views.send),
+    path('add', views.add),
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,5 @@
+import re
+
 from django.shortcuts import render, redirect
 from wakeonlan import send_magic_packet
 
@@ -16,3 +18,18 @@ def send(request):
     mac_address = request.GET['mac_address']
     send_magic_packet(mac_address)
     return redirect('/')
+
+
+def add(request):
+    mac_address = request.GET['mac_address']
+    name = request.GET['name']
+
+    # Checks for valid mac address.
+    is_valid_mac = re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", mac_address.lower())
+    is_valid_name = len(name) < 100
+    if is_valid_mac and is_valid_name:
+        computer = Computer(name=name, mac_address=mac_address)
+        computer.save()
+        return redirect('/')
+    # else:
+        # return render_template('error_format.jinja2', mac_address=mac_address)
